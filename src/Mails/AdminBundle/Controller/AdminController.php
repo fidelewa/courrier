48aa91977acfcce62cff00dacf76bdf5c5dddd80
fidelewa <fiderlet07@gmail.com>
2016-08-25 00:20:56 +0000

    Réaménagement des actions du contrôleur

--- src/Mails/AdminBundle/Controller/AdminController.php
+++ src/Mails/AdminBundle/Controller/AdminController.php
@@ -122,7 +122,7 @@ class AdminController extends Controller
             'page' => $page,
             ));
     }
-//--------------------------------------------------------------------------    
+    //--------------------------------------------------------------------------    
     /**
      * Edit mail sent controller.
      *
@@ -283,7 +283,7 @@ class AdminController extends Controller
         // Puis on redirige vers l'accueil
         return $this->redirect($this->generateUrl('mails_admin_mailreceived'));
     }
- //------------------------------------------------------------------------------------   
+    //------------------------------------------------------------------------------------   
     /**
      * Edit actor controller.
      *
@@ -408,7 +408,7 @@ class AdminController extends Controller
         // Puis on redirige vers l'accueil
         return $this->redirect($this->generateUrl('mails_admin_actor'));
     }
-//----------------------------------------------------------------------------------------    
+    //----------------------------------------------------------------------------------------    
     /**
      * Delete user controller.
      *
@@ -500,7 +500,7 @@ class AdminController extends Controller
         // Puis on redirige vers l'accueil
         return $this->redirect($this->generateUrl('mails_admin_user'));
     }
-    //-------------------------------------------------------------------------------------
+     //------------------------------------------------------------
      public function filterMailsentAction(Request $request)
      {
         //On crée le mail
@@ -641,6 +641,84 @@ class AdminController extends Controller
         'form' => $form->createView()
         ));
          
+     }
+
+     public function filterAllMailsentAction(Request $request, $page)
+     {
+         if ($page < 1) {
+        throw new NotFoundHttpException('Page "'.$page.'" inexistante.');
+        }
+         
+        //On crée le mail
+        $mail = new Mail();
+        
+        //On crée notre formulaire
+        $form = $this->createForm(new MailSentFilterType(), $mail);
+         
+        //Si la requête est en POST on affiche la liste du resultat de la recherche
+        if($form->handleRequest($request)->isValid()) 
+        {
+            // On récupère les données du courrier envoyé
+            $mail = $form->getData();
+            $days = $mail->getNbDaysBefore();
+            $reception = $mail->getReceived();
+            $destinataire = $mail->getMailsent()->getActor()->getName();
+            $expediteur = $this->getUser()->getUsername();
+            
+            // On récupère notre service
+            $filter = $this->get('mails_admin.mail_filter');
+
+            //On récupère tous les courriers envoyés, filtrés par date et par reception
+            $allMailsentFilter = $filter->filtreAllMailsent($days, $reception, $expediteur, $destinataire, $page, $filter::NUM_ITEMS);
+
+            // On calcule le nombre total de pages grâce au count($listMailsSent) qui retourne le nombre total de courriers envoyé
+            $nombreTotalMailsSent = $allMailsentFilter->count();
+            $nombreMailsentPage = $filter::NUM_ITEMS;
+            $nombreTotalPages = ceil($nombreTotalMailsSent/$nombreMailsentPage); 
+        
+            if($page > $nombreTotalPages){
+                throw $this->createNotFoundException("Aucune donnée ne correspond a cette recherche !");
+            }
+
+            return $this->render('MailsAdminBundle:Admin:all_mailsent_filter_result.html.twig', array(
+            'allMailsentFilter' => $allMailsentFilter,
+            'mail' => $mail,
+            'nbPages' => $nombreTotalPages,
+            'page' => $page,
+            ));
+
+        }
+        
+        //Si la requête est en GET on affiche le formulaire de critère de recherche
+        return $this->render('MailsAdminBundle:Admin:all_mailsent_filter.html.twig', array(
+        'form' => $form->createView()
+        ));
+         
+     }
+
+     public function validateMailsentAction($id, Request $request)
+     {
+        //On récupère notre Entity Manager 
+        $em = $this->getDoctrine()->getManager();
+
+        // On récupère l'$id du mail sent 
+        $mailsent = $em->getRepository('MailsMailBundle:Mail')->findMailSent($id);
+
+        if (null === $mailsent) {
+        throw new NotFoundHttpException("Le courrier envoyé d'id ".$id." n'existe pas.");
+        }
+        
+        //On valide le mail sent
+        $mailsent->setValidated(true);
+        
+        // Inutile de persister ici, Doctrine connait déja notre courrier envoyé
+        $em->flush();
+       
+        //On redirige vers la page d'accueil
+        $request->getSession()->getFlashBag()->add('success', 'Le courrier envoyé de référence "'.$mailsent->getReference().'" a bien été validé.');
+
+        return $this->redirect($this->generateUrl('mails_core_home'));
+                
      }
 
      //-------------------------------------------------------------------------------------
@@ -787,60 +865,7 @@ class AdminController extends Controller
         ));
          
      }
-     //--------------------------------------------------------------------------------------
-     public function filterAllMailsentAction(Request $request, $page)
-     {
-         if ($page < 1) {
-        throw new NotFoundHttpException('Page "'.$page.'" inexistante.');
-        }
-         
-        //On crée le mail
-        $mail = new Mail();
-        
-        //On crée notre formulaire
-        $form = $this->createForm(new MailSentFilterType(), $mail);
-         
-        //Si la requête est en POST on affiche la liste du resultat de la recherche
-        if($form->handleRequest($request)->isValid()) 
-        {
-            // On récupère les données du courrier envoyé
-            $mail = $form->getData();
-            $days = $mail->getNbDaysBefore();
-            $reception = $mail->getReceived();
-            $destinataire = $mail->getMailsent()->getActor()->getName();
-            $expediteur = $this->getUser()->getUsername();
-            
-            // On récupère notre service
-            $filter = $this->get('mails_admin.mail_filter');
 
-            //On récupère tous les courriers envoyés, filtrés par date et par reception
-            $allMailsentFilter = $filter->filtreAllMailsent($days, $reception, $expediteur, $destinataire, $page, $filter::NUM_ITEMS);
-
-            // On calcule le nombre total de pages grâce au count($listMailsSent) qui retourne le nombre total de courriers envoyé
-            $nombreTotalMailsSent = $allMailsentFilter->count();
-            $nombreMailsentPage = $filter::NUM_ITEMS;
-            $nombreTotalPages = ceil($nombreTotalMailsSent/$nombreMailsentPage); 
-        
-            if($page > $nombreTotalPages){
-                throw $this->createNotFoundException("Aucune donnée ne correspond a cette recherche !");
-            }
-
-            return $this->render('MailsAdminBundle:Admin:all_mailsent_filter_result.html.twig', array(
-            'allMailsentFilter' => $allMailsentFilter,
-            'mail' => $mail,
-            'nbPages' => $nombreTotalPages,
-            'page' => $page,
-            ));
-
-        }
-        
-        //Si la requête est en GET on affiche le formulaire de critère de recherche
-        return $this->render('MailsAdminBundle:Admin:all_mailsent_filter.html.twig', array(
-        'form' => $form->createView()
-        ));
-         
-     }
-     
      public function filterAllMailreceivedAction(Request $request, $page)
      {
          if ($page < 1) {
@@ -894,32 +919,7 @@ class AdminController extends Controller
         ));
          
      }
-     
-     public function validateMailsentAction($id, Request $request)
-     {
-        //On récupère notre Entity Manager 
-        $em = $this->getDoctrine()->getManager();
 
-        // On récupère l'$id du mail sent 
-        $mailsent = $em->getRepository('MailsMailBundle:Mail')->findMailSent($id);
-
-        if (null === $mailsent) {
-        throw new NotFoundHttpException("Le courrier envoyé d'id ".$id." n'existe pas.");
-        }
-        
-        //On valide le mail sent
-        $mailsent->setValidated(true);
-        
-        // Inutile de persister ici, Doctrine connait déja notre courrier envoyé
-        $em->flush();
-       
-        //On redirige vers la page d'accueil
-        $request->getSession()->getFlashBag()->add('success', 'Le courrier envoyé de référence "'.$mailsent->getReference().'" a bien été validé.');
-
-        return $this->redirect($this->generateUrl('mails_core_home'));
-                
-     }
-     
      public function validateMailreceivedAction($id, Request $request)
      {
         //On récupère notre Entity Manager 
