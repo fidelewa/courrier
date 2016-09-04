3adb6ff0bf24fca45b9ab9c3b309caaf6271b246
fidelewa <fiderlet07@gmail.com>
2016-09-04 11:58:49 +0000

    Modif

--- web/app_dev.php
+++ web/app_dev.php
@@ -24,7 +24,6 @@ Debug::enable();
 require_once __DIR__.'/../app/AppKernel.php';
 
 $kernel = new AppKernel('dev', true);
-$kernel->boot();
 $kernel->loadClassCache();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
