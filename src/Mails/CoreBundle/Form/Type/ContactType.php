616bca6e60f3ba453cabd70358efa91d5aa21c43
fidelewa <fiderlet07@gmail.com>
2016-10-09 18:04:19 +0000

    Créer le formulaire "ContactType"

---
+++ src/Mails/CoreBundle/Form/Type/ContactType.php
@@ -0,0 +1,22 @@
+<?php
+# src/tuto/WelcomeBundle/Form/Type/ContactType.php
+
+namespace Mails\CoreBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('email', 'email')
+                ->add('subject', 'text')
+                ->add('content', 'textarea');
+    }
+
+    public function getName()
+    {
+        return 'Contact';
+    }
+}
