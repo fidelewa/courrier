af5a1d4d1d386a4e4d472ee855c31665b67b2e81
fidelewa <fiderlet07@gmail.com>
2016-11-13 13:59:11 +0000

    Ajouter la relation entre User et Company

--- src/Mails/UserBundle/Entity/User.php
+++ src/Mails/UserBundle/Entity/User.php
@@ -4,6 +4,7 @@ namespace Mails\UserBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use FOS\UserBundle\Model\User as BaseUser;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity
@@ -16,4 +17,35 @@ class User extends BaseUser
    * @ORM\GeneratedValue(strategy="AUTO")
    */
     protected $id;
+
+    /**
+    * @ORM\ManyToOne(targetEntity="Mails\MailBundle\Entity\Company")
+    * @ORM\JoinColumn(nullable=true)
+    * @Assert\Valid
+    */
+    private $company;
+
+    /**
+     * Set company
+     *
+     * @param \Mails\MailBundle\Entity\Company $company
+     *
+     * @return User
+     */
+    public function setCompany(\Mails\MailBundle\Entity\Company $company = null)
+    {
+        $this->company = $company;
+
+        return $this;
+    }
+
+    /**
+     * Get company
+     *
+     * @return \Mails\MailBundle\Entity\Company
+     */
+    public function getCompany()
+    {
+        return $this->company;
+    }
 }
