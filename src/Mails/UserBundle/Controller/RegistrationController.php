345ad09bff8d59e0afd1871846810427ee0a069f
fidelewa <fiderlet07@gmail.com>
2016-11-15 23:04:23 +0000

    Supprimer l'usage du controller use Symfony\Bundle\FrameworkBundle\Controller\Controller;

--- src/Mails/UserBundle/Controller/RegistrationController.php
+++ src/Mails/UserBundle/Controller/RegistrationController.php
@@ -6,7 +6,6 @@ use FOS\UserBundle\FOSUserEvents;
 use FOS\UserBundle\Event\FormEvent;
 use FOS\UserBundle\Event\GetResponseUserEvent;
 use FOS\UserBundle\Event\FilterUserResponseEvent;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use FOS\UserBundle\Controller\RegistrationController as BaseRegistration;
